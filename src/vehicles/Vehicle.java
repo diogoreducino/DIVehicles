f85ee8bbbc7c7a96ace3054a2fd7042ab5312fe3
diogo <diogo@DESKTOP-F47R9TB>
2016-10-13 20:43:00 -0300

    Criação de classe "Vehicle"

---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles;
+
+/**
+ *
+ * @author diogo
+ */
+public class Vehicle {
+    private double gallonsOfGas;
+    private int mpg;
+
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    public int getMpg() {
+        return mpg;
+    }
+
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
+    
+    
+}
